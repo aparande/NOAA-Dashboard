6e059eef24a38b8a65c6b71975b163d6de48164f
lelzeiny <leena.elzeiny@gmail.com>
2021-03-20 22:50:06 -0700

    full set up

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "www")));
 
 const server = app.listen(8000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
