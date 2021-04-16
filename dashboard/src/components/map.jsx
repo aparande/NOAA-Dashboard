04f83784d292612094808ebcffbcc2259a2b602a
ehildebrandtrojo <edgarhr.usa@gmail.com>
2021-04-15 19:24:00 -0700

    Added slider + buttons

--- dashboard/src/components/map.jsx
+++ dashboard/src/components/map.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { get_all_traces, get_oil_gas_platforms } from '../queries';
 import { MapContainer, TileLayer, ImageOverlay} from 'react-leaflet';
 import Buoy from './buoy';
+import Labeled from './Labeled';
 import OilPlatform from './oil_platform';
 
 // This is the API Key from MapBox documentation. Might as well just use it for now
@@ -14,6 +15,7 @@ const Map = () => {
   const [traces, setTraces] = useState({});
   const [currTime, setCurrTime] = useState(1535623127);
   const [platformLocs, setPlatformLocs] = useState([]);
+  const [step, setStep] = useState(365);
 
   // Effect to load the data when the app first loads
   useEffect(() => {
@@ -34,21 +36,33 @@ const Map = () => {
   }, [])
 
   return (
-    <MapContainer center={[lat, lng]} zoom={zoom} style={{ width: '100%', height: '100vh'}} >
-      <TileLayer
-        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* Can tweak the coordinates to make it overlap better */}
-      <ImageOverlay bounds={[[29.75, -141.540], [47.75, -108.610]]} url="/sea-lion-data.png"/>
-      { 
-        Object.keys(traces).map((key, idx) => 
-        <Buoy currTime={ currTime } drift_num={ key } 
-              positions={ traces[key] } key={ idx } 
-              setCurrTime = { setCurrTime } />)
-      }
-      { platformLocs.map((b, idx) => <OilPlatform platform={b} key={"platform" + idx} />) }
+    <div>
+      <MapContainer center={[lat, lng]} zoom={zoom} style={{ width: '100%', height: '100vh'}} >
+        <TileLayer
+          attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Can tweak the coordinates to make it overlap better */}
+        <ImageOverlay bounds={[[29.75, -141.540], [47.75, -108.610]]} url="/sea-lion-data.png"/>
+        {
+          Object.keys(traces).map((key, idx) =>
+          <Buoy currTime={ currTime } drift_num={ key }
+                positions={ traces[key] } key={ idx }
+                setCurrTime = { setCurrTime } />)
+        }
+        { platformLocs.map((b, idx) => <OilPlatform platform={b} key={"platform" + idx} />) }
       </MapContainer>
+      <div>
+        <div className="sliderwrapper">
+          <Labeled className="slider" step={ step } minTime={ minTime } maxTime={ maxTime } currTime={ currTime } setCurrTime = { setCurrTime } />
+        </div>
+        <div className="buttons">
+          <button className="button" onClick={() => setStep(1)}>Day</button>
+          <button className="button" onClick={() => setStep(7)}>Week</button>
+          <button className="button" onClick={() => setStep(30)}>Month</button>
+        </div>
+      </div>
+    </div>
   )};
 
 export default Map;
