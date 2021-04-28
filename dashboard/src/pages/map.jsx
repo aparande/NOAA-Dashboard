ea3882e7f8174caa6b2013f3ab63aa1aef4549b7
Anmol Parande <parandea1.7@gmail.com>
2021-04-28 12:54:41 -0700

    Set min and max map zoom

--- dashboard/src/pages/map.jsx
+++ dashboard/src/pages/map.jsx
@@ -137,7 +137,7 @@ const Map = () => {
 
   return (
     <>
-    <MapContainer center={[36.7783, -119.4179]} zoom={6} style={{ width: '100%', height: '100vh'}} >
+    <MapContainer center={[36.7783, -119.4179]} zoom={7} maxZoom={8} minZoom={5} style={{ width: '100%', height: '100vh'}} >
       <TileLayer
         attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
