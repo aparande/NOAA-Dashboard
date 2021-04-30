b290f423867ddc80bf4cca09f80e3c9ea5350873
Anmol Parande <parandea1.7@gmail.com>
2021-04-30 11:43:13 -0700

    Added Crosshair to BB chart

--- dashboard/src/components/trace_popup.jsx
+++ dashboard/src/components/trace_popup.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import 'react-vis/dist/style.css';
-import {XYPlot, LineSeries, VerticalBarSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines} from 'react-vis';
+import {XYPlot, LineSeries, Crosshair, XAxis, YAxis, VerticalGridLines, HorizontalGridLines} from 'react-vis';
+import Loader from 'react-loader-spinner';
+import { usePromiseTracker, trackPromise } from 'react-promise-tracker';
+
 import { get_bb } from '../queries';
 
 const TracePopup = (props) => {
   const [bbData, setBBData] = useState(null);
+  const [crosshairPoint, setCrosshairPoint] = useState([]);
+
+  const { promiseInProgress } = usePromiseTracker({ area: "trace-popup-area" });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,12 +24,28 @@ const TracePopup = (props) => {
         setBBData(null);
       }
     }
-    if (props.isOpen) fetchData();
+    if (props.isOpen) trackPromise(fetchData(), "trace-popup-area");
   }, [props.isOpen, props.minTime, props.maxTime, props.step, props.drift_num]);
 
-  // return <p>No Data Found</p>
-  if (bbData === null || bbData === undefined) {
-    return <p>No Data Found</p>
+  useEffect(() => {
+    if (bbData === null || bbData === undefined || !props.isOpen || props.currTime === null || props.currTime === undefined) {
+			setCrosshairPoint([]);
+			return;
+		}
+
+    for (let i = 0; i < bbData.length - 1; i++) {
+      if (parseInt(bbData[i].x) > props.currTime) continue;
+      if (parseInt(bbData[i].x) <= props.currTime && parseInt(bbData[i + 1].x) >= props.currTime) {
+        setCrosshairPoint([bbData[i]]);
+        return;
+      }
+    }
+  }, [bbData, props.currTime, props.isOpen]);
+
+  if (promiseInProgress) {
+    return <Loader type="ThreeDots" color="#212529" visible/>
+  } else if (bbData === null || bbData === undefined) {
+    return <p>Missing data for buoy. It will be availble soon</p>
   } else {
     return (
       <div>
@@ -38,11 +60,17 @@ const TracePopup = (props) => {
           tickFormat={function tickFormat(d){return new Date(d * 1000).toLocaleDateString()}}
           tickLabelAngle={-46} />
         <YAxis/>
-        <LineSeries data={bbData} />
+        <LineSeries data={bbData} /> 
+			  <Crosshair style={{ line: {background: '#212428', width: '3px' }}} 
+                  values={crosshairPoint} itemsFormat={(values) => values.map((pt) => {
+                    return { title: "BB", value: `${pt.y.toFixed(2)} dB` }
+                    })
+                  } titleFormat={(values) => { return  { title: "Time", value: (new Date(values[0].x * 1000)).toLocaleString() }}} />
+				
       </XYPlot>
       </div>
     )
   }
-}
+};
 
 export default TracePopup;
