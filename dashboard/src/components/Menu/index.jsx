9d07f95396989a5a589c8a5960ef5a7183c71a98
Anmol Parande <parandea1.7@gmail.com>
2021-06-29 20:56:05 -0500

    Merged RadioMenuItem and MenuItem components

--- dashboard/src/components/Menu/index.jsx
+++ dashboard/src/components/Menu/index.jsx
@@ -35,30 +35,6 @@ const LayerItemRadio = ({ value, onSelect, checked }) => {
     )
 }
 
-// TODO: Make RadioMenuItem and MenuItem the same
-const RadioMenuItem = ({ title, onChange, values, defaultValue }) => {
-    const [isActive, setIsActive] = useState(false);
-    const [value, setValue] = useState(null);
-
-    useEffect(() => onChange(value), [value, onChange]);
-    useEffect(() => { if (value === null || value === undefined) setValue(defaultValue) }, [ defaultValue, value ] );
-
-    return (
-        <div className={styles.item}>
-            <div className={styles.title} onClick={() => setIsActive(!isActive)} >
-                {icons[title]}
-                <div className={styles.itemHeader}>{title}</div>
-                <div style={{lineHeight: '30px'}}>{isActive ? '-' : '+'}</div>
-            </div>
-            {isActive &&
-                <div className={styles.itemContent}>
-                    { values.map((val, i) => <LayerItemRadio value={val} key={i} onSelect={() => setValue(val) } checked={value === val}/>) }
-                </div>
-            }
-        </div>
-    )
-}
-
 const MenuItem = (props) => {
     const [isActive, setIsActive] = useState(false);
 
@@ -78,25 +54,34 @@ const MenuItem = (props) => {
     );
 }
 
-const Menu = (props) => {
+const Menu = ({ layers }) => {
+    const [habitatValue, setHabitatValue] = useState("None");
+
+    useEffect(() => layers("Habitats", "selectedHabitat", habitatValue), [ habitatValue, layers ]);
+
     return(
         <div className={styles.container}>
-            <MenuItem title="Buoys" toggle={props.layers}>
-                <LayerItemCheckbox name="Buoy Path" toggle={(value) => props.layers("Buoys", "BuoyPath", value)} checked={true}/>
+            <MenuItem title="Buoys">
+                <LayerItemCheckbox name="Buoy Path" toggle={(value) => layers("Buoys", "BuoyPath", value)} checked={true}/>
             </MenuItem>
-            <MenuItem title="Anthropogenic" toggle={props.layers}>
-                <LayerItemCheckbox name="Oil Rigs" toggle={(value) => props.layers("Development", "OilRigs", value)} checked={false}/>
-                <LayerItemCheckbox name="Shipping Routes" toggle={(value) => props.layers("Development", "ShippingRoutes", value)} checked={false}/>
+            <MenuItem title="Anthropogenic">
+                <LayerItemCheckbox name="Oil Rigs" toggle={(value) => layers("Development", "OilRigs", value)} checked={false}/>
+                <LayerItemCheckbox name="Shipping Routes" toggle={(value) => layers("Development", "ShippingRoutes", value)} checked={false}/>
             </MenuItem>
-            <MenuItem title="Detections" toggle={props.layers}>
+            <MenuItem title="Detections">
                 {
                     Object.keys(SPECIES_DETECTION_KEYS).map((key, idx) =>
-                        <LayerItemCheckbox name={SPECIES_DETECTION_KEYS[key]} toggle={(value) => props.layers("Detections", key, value)} checked={false} key={idx}/>
+                        <LayerItemCheckbox name={SPECIES_DETECTION_KEYS[key]} toggle={(value) => layers("Detections", key, value)} checked={false} key={idx}/>
                     )
                 }
             </MenuItem>
-            <RadioMenuItem title="Habitats" onChange={(value) => props.layers("Habitats", "selectedHabitat", value)}
-                           values={ ["None", ...SPECIES_HABITAT_KEYS] } defaultValue="None"/>
+            <MenuItem title="Habitats">
+                {
+                    ["None", ...SPECIES_HABITAT_KEYS].map((val, i) => 
+                        <LayerItemRadio value={val} key={i} onSelect={() => setHabitatValue(val) } checked={habitatValue === val}/>
+                    )
+                }
+            </MenuItem>
         </div>
     );
 }
