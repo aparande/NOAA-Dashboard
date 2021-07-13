58033dff76be37e516c77f0747ddd167976d8c9b
Anmol Parande <parandea1.7@gmail.com>
2021-07-12 23:39:44 -0500

    Fixed spelling issues

--- dashboard/src/pages/Map/onboarding_steps.jsx
+++ dashboard/src/pages/Map/onboarding_steps.jsx
@@ -2,32 +2,32 @@ import styles from "./map.module.css";
 import "../../styles/leaflet.css";
 
 export const step_var =[{
-    content: <div><h2>Welcome to the CalSound tool!</h2><p>This tool is meant to help visualize the impacts of manmade noise pollution in the ocean and provides a clear way of visualizing ocean soundscapes.</p></div>,
+    content: <div><h2>Welcome to CalSound!</h2><p>This tool is meant to help visualize the impacts of manmade noise pollution in the ocean and provide a clear way of visualizing ocean soundscapes.</p></div>,
     position: 'center'
   },
   {
-    content: <div className={styles.stepContent}><img src="/images/buoy_path.jpg" className='photo' width='100%' height='100%' /><p>The markers in the ocean are buoys placed by NOAA to collect data on sound and the grey lines are the paths the buoys have taken over time</p></div>,
+    content: <div className={styles.stepContent}><img src="/images/buoy_path.jpg" className='photo' width='100%' height='100%' /><p>The markers in the ocean are buoys placed by NOAA to collect data on sound, and the grey lines are the paths the buoys have taken over time.</p></div>,
     floaterProps: {
       disableAnimation: true,
     },
     spotlightPadding: 20
   },
   {
-    content: <div className={styles.stepContent}><img src="/images/hover_path.jpg" className='photo' width='100%' height='100%' /><p>Hover over the path to find out the history of noise levels in that spot, also known as the broadband measurement</p></div>,
+    content: <div className={styles.stepContent}><img src="/images/hover_path.jpg" className='photo' width='100%' height='100%' /><p>Hover over the path to find out see the Broadband 20Hz - 20kHz measurement over the lifecycle of the buoy.</p></div>,
     floaterProps: {
       disableAnimation: true,
     },
     spotlightPadding: 20,
   },
   {
-    content: <div className={styles.stepContent}><img src="/images/click_buoy.jpg" className='photo' width='100%' height='100%' /><p>Click on the buoy itself to see the third octave level, which divides up the sound into different frequencies shows the amount of noise for each frequency.</p></div>,
+    content: <div className={styles.stepContent}><img src="/images/click_buoy.jpg" className='photo' width='100%' height='100%' /><p>Click on the buoy itself to see third octave level measurements, which show how much noise energy exists in different frequency bands.</p></div>,
     floaterProps: {
       disableAnimation: true,
     },
     spotlightPadding: 20
   },
   {
-    content: <p>See how the sound data changes over time by scrubbing through the time line on the left, or by changing the window of time on the right</p>,
+    content: <p>See how the sound data changes over time by scrubbing through the timeline. The window of time for which you are seeing the data is changed via the different buttons.</p>,
     floaterProps: {
       disableAnimation: true,
     },
@@ -35,7 +35,7 @@ export const step_var =[{
     selector: '#slider',
   },
   {
-    content: <p>Discover the impact of these noise levels by enabling layers, and clicking on the points that appear</p>,
+    content: <p>Discover the impact of these noise levels by enabling different layers such as habitats or species detections.</p>,
     floaterProps: {
       disableAnimation: true,
     },
@@ -44,6 +44,6 @@ export const step_var =[{
     resizeObservables: ['#menu']
   },
   {
-    content: <div><h2>Start Exploring!</h2><p>Check out the Resources tab for information on the source of the data on this map or for relevant links on soundscapes</p></div>,
+    content: <div><h2>Start Exploring!</h2><p>Check out the Resources tab for information on the source of the data on this map or for relevant links on soundscapes.</p></div>,
     position: 'center'
   }];
